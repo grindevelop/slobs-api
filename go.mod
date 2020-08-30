5e117ff0c9e26ab77ec80a5d30803502bc891d4c
grindevelop <grindevelop@gmail.com>
2020-08-30 19:39:18 +0300

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/grindevelop/slobs-api
 
 go 1.15
+
+require (
+	github.com/buger/jsonparser v1.0.0
+	github.com/gorilla/websocket v1.4.2
+)
--- slobsapi.go
+++ slobsapi.go
@@ -1,6 +1,14 @@
 package slobsapi
 
-type RPCRequest struct {
+import (
+	"errors"
+
+	"github.com/buger/jsonparser"
+
+	"github.com/gorilla/websocket"
+)
+
+type rpcRequest struct {
 	JSONRPC string      `json:"jsonrpc"`
 	ID      int         `json:"id"`
 	Method  string      `json:"method"`
@@ -13,27 +21,79 @@ type params struct {
 	CompactMode bool          `json:"compactMode,omitempty"`
 }
 
-func Request(id int, method, resource string, args ...interface{}) *RPCRequest {
-	return &RPCRequest{
-		JSONRPC: "2.0",
-		ID:      id,
-		Method:  method,
-		Params: &params{
-			Resource: resource,
-			Args:     args,
-		},
-	}
+type RemoteConn struct {
+	WebSocket *websocket.Conn
+	response  chan []byte
+	id        int
 }
 
-func RequestCompact(id int, method, resource string, args ...interface{}) *RPCRequest {
-	return &RPCRequest{
+func request(id int, method, resource string, args []interface{}, compact bool) *rpcRequest {
+	return &rpcRequest{
 		JSONRPC: "2.0",
 		ID:      id,
 		Method:  method,
 		Params: &params{
 			Resource:    resource,
 			Args:        args,
-			CompactMode: true,
+			CompactMode: compact,
 		},
 	}
+}
+
+func (rc *RemoteConn) nextID() int {
+	rc.id++
+	if rc.id == 0 {
+		rc.id++
+	}
+	return rc.id
+}
+
+func (rc *RemoteConn) ListenEvents(receive chan []byte) error {
+	rc.response = make(chan []byte)
+	for {
+		_, message, err := rc.WebSocket.ReadMessage()
+		if err != nil {
+			return err
+		}
+		if s, _ := jsonparser.GetString(message, "result", "_type"); s == "EVENT" {
+			result, _, _, _ := jsonparser.Get(message, "result")
+			receive <- result
+		} else {
+			rc.response <- message
+		}
+	}
+}
+
+func (rc *RemoteConn) send(request *rpcRequest) ([]byte, error) {
+	err := rc.WebSocket.WriteJSON(request)
+	if err != nil {
+		return nil, err
+	}
+	var response []byte
+	if rc.response != nil {
+		response = <-rc.response
+	} else {
+		err := rc.WebSocket.ReadJSON(&response)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if msg, err := jsonparser.GetString(response, "error", "message"); err == nil {
+		return nil, errors.New(msg)
+	}
+	result, _, _, _ := jsonparser.Get(response, "result")
+	return result, nil
+}
+
+func (rc *RemoteConn) Call(method, resource string, args ...interface{}) ([]byte, error) {
+	return rc.send(request(rc.nextID(), method, resource, args, false))
+}
+
+func (rc *RemoteConn) CallCompact(method, resource string, args ...interface{}) ([]byte, error) {
+	return rc.send(request(rc.nextID(), method, resource, args, true))
+}
+
+func (rc *RemoteConn) Notify(method, resource string, args ...interface{}) error {
+	err := rc.WebSocket.WriteJSON(request(rc.nextID(), method, resource, args, false))
+	return err
 }
